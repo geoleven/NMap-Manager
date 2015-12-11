9228529afc97cda202bc61056ffd67ce848657cd
George Pittas <sdi1000122@di.uoa.gr>
2015-12-11 23:39:55 +0200

    Setting up AM and SA webservices

--- NmapProject.SA/src/main/java/gr/uoa/di/NmapProject/SA/Main.java
+++ NmapProject.SA/src/main/java/gr/uoa/di/NmapProject/SA/Main.java
@@ -1,36 +1,57 @@
 package gr.uoa.di.NmapProject.SA;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import org.json.simple.JSONObject;
+
 public class Main {
 
 	public static void main(String[] args) {
 		try {
 			
-			Registration myr = new Registration();
+//			Registration myr = new Registration();
 			
-			
-			Globals.verbose = true;
-			if (!MySystemFiles.checkProgramPathExists()) {
-				if (Globals.verbose)
-					System.err.println("The deamon cannot run without its directory '~/.myNmap'");
-				return;
-			}
-			SAProperties myProperties = new SAProperties();
-			myProperties.readThreadNum();
-			// System.out.println(myp.oneTimeJobThreadsNumber);
+			Client client = Client.create();
+            WebResource webResource = client
+                    .resource("http://localhost:8080/myapp/music/get");
 
-			JobQueue jQ = new JobQueue();
-			ResultQueue rQ = new ResultQueue();
-			PeriodicJobs pj = new PeriodicJobs(rQ);
-			GetPendingJobs jf = new GetPendingJobs(jQ, pj);
-			SendResults sT = new SendResults(rQ);
-			OneTimeJobs otj = new OneTimeJobs(jQ, rQ);
+            ClientResponse response = webResource
+                    .accept("application/json")
+                    .get(ClientResponse.class);
 
-			Stopper stopper = new Stopper(jf, otj, pj, sT);
-			stopper.attachShutDownHook();
+            if (response.getStatus() != 200)
+                throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
+
+            String output = response.getEntity(String.class);
+
+            System.out.println("Output from Server .... \n");
+            System.out.println(output);
 			
-			otj.start();
-			jf.start();
-			sT.start();			
+			
+//			Globals.verbose = true;
+//			if (!MySystemFiles.checkProgramPathExists()) {
+//				if (Globals.verbose)
+//					System.err.println("The deamon cannot run without its directory '~/.myNmap'");
+//				return;
+//			}
+//			SAProperties myProperties = new SAProperties();
+//			myProperties.readThreadNum();
+//			// System.out.println(myp.oneTimeJobThreadsNumber);
+//
+//			JobQueue jQ = new JobQueue();
+//			ResultQueue rQ = new ResultQueue();
+//			PeriodicJobs pj = new PeriodicJobs(rQ);
+//			GetPendingJobs jf = new GetPendingJobs(jQ, pj);
+//			SendResults sT = new SendResults(rQ);
+//			OneTimeJobs otj = new OneTimeJobs(jQ, rQ);
+//
+//			Stopper stopper = new Stopper(jf, otj, pj, sT);
+//			stopper.attachShutDownHook();
+//			
+//			otj.start();
+//			jf.start();
+//			sT.start();			
 			
 			
 		} catch (Exception e) {
