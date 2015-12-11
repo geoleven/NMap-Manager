156931ef7f3620c6af67f85eb3a42540acc986b6
Giorgos Leventis <sdi1000210@di.uoa.gr>
2015-12-11 14:09:39 +0200

    Added MA folder and structure.

---
+++ NmapProject.MA/src/main/java/gr/uoa/di/NmapProject/MA/App.java
@@ -0,0 +1,13 @@
+package gr.uoa.di.NmapProject.MA;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
