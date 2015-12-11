56f1b70608340383ecbb8283b91ec6a590780a49
Giorgos Leventis <sdi1000210@di.uoa.gr>
2015-12-11 18:28:24 +0200

    Started the classes for acquiring and send Registration info.

--- NmapProject.SA/src/main/java/gr/uoa/di/NmapProject/SA/Main.java
+++ NmapProject.SA/src/main/java/gr/uoa/di/NmapProject/SA/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		try {
+			
+			Registration myr = new Registration();
+			
+			
 			Globals.verbose = true;
 			if (!MySystemFiles.checkProgramPathExists()) {
 				if (Globals.verbose)
