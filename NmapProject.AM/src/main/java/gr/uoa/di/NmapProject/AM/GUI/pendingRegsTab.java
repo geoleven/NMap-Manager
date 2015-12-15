2b7f1c468b8ec3104cd9a04294ac72de76238176
Giorgos Leventis <sdi1000210@di.uoa.gr>
2015-12-15 20:53:38 +0200

    Added scroll pane before list

--- NmapProject.AM/src/main/java/gr/uoa/di/NmapProject/AM/GUI/pendingRegsTab.java
+++ NmapProject.AM/src/main/java/gr/uoa/di/NmapProject/AM/GUI/pendingRegsTab.java
@@ -14,13 +14,14 @@ public class pendingRegsTab {
 	public static void populatePendingRegList(JList<JCheckBox> checkList) {
 		LinkedList<SA> curList = AdminPanelDAO.getPendReg();
 		System.out.println(curList.size());
+		//currentHeight
 		for (SA curSA : curList) {
 			JCheckBox tempcb = new JCheckBox(curSA.id + "\t" + curSA.device_name + "\t" + curSA.ip + "\t" + curSA.mac_address + "\t" + curSA.os_version + "\t" + curSA.nmap_version + "\t" + curSA.hash);
 			tempcb.setHorizontalAlignment(SwingConstants.CENTER);
 			//tempcb.setActionCommand("setToReg");
 			tempcb.setHorizontalTextPosition(SwingConstants.LEADING);
 			tempcb.setFont(new Font("Tahoma", Font.PLAIN, 12));
-			tempcb.setBounds(0, 0, 700, 50);
+			//tempcb.setBounds(0, 0, 750, 50);
 			checkList.add(tempcb);
 			
 		    int index = checkList.getMaxSelectionIndex(); //get selected index
@@ -31,6 +32,7 @@ public class pendingRegsTab {
 		    }
 			checkList.setSelectedIndex(index);
 			checkList.ensureIndexIsVisible(index);
+			checkList.updateUI();
 			
 			System.out.println(curSA.id + "\t" + curSA.device_name + "\t" + curSA.ip + "\t" + curSA.mac_address + "\t" + curSA.os_version + "\t" + curSA.nmap_version + "\t" + curSA.hash);
 		}
