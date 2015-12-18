bcf1761b362921f8d9904883d86a2252144cc17e
Giorgos Leventis <sdi1000210@di.uoa.gr>
2015-12-18 07:26:59 +0200

    Added xanal depedency to pom.xml. Minor other changes.

--- NmapProject.AM/src/main/java/gr/uoa/di/NmapProject/AM/Globals.java
+++ NmapProject.AM/src/main/java/gr/uoa/di/NmapProject/AM/Globals.java
@@ -1,6 +1,9 @@
 package gr.uoa.di.NmapProject.AM;
 
+import java.util.LinkedList;
+
 public class Globals {
 	public static final int refreshRate = 2;
 	public static final boolean verbose = true;
+	public LinkedList<String> onlineSAs = new LinkedList<String>();
 }
--- NmapProject.AM/src/main/java/gr/uoa/di/NmapProject/AM/Main.java
+++ NmapProject.AM/src/main/java/gr/uoa/di/NmapProject/AM/Main.java
@@ -6,8 +6,9 @@ import gr.uoa.di.NmapProject.AM.GUI.App;
  * Main class.
  *
  */
-public class Main {
+public class Main {	
 	public static void main(String[] args){
+		Globals globals = new Globals();
 		App app = new App();
 		app.run();
 	}
